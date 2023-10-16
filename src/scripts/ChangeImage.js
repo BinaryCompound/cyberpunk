b759c6520133e3aef75bb83629b06c20e105ee06
Ilya Pozvolenko <BinaryCompound2@gmail.com>
2023-10-16 17:28:54 +0300

    addet change background image

--- src/scripts/ChangeImage.js
+++ src/scripts/ChangeImage.js
@@ -2,6 +2,26 @@ import Cyber from "../../public/1.jpg";
 import CyberTwo from "../../public/2.png";
 import Cyberthree from "../../public/3.jpg";
 
+const images = [
+    '',
+    'CyberTwo.png',
+    'CyberThree.jpg',
+]
+
+let currentIndex = 0;
+
+// Функция для изменения фонового изображения
+function changeBackgroundImage() {
+    document.querySelector('.background-image').style.backgroundImage = `url(${images[currentIndex]})`;
+
+    // Увеличиваем индекс на 1, и переходим к следующему изображению
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+// Вызываем функцию смены изображения через определенный интервал времени
+setInterval(changeBackgroundImage, 5000);
+
+/*
 let CyberImage = [
     { image: Cyber },
     { image: CyberTwo },
@@ -23,3 +43,4 @@ function changeImage() {
 
 // Устанавливаем интервал для автоматической смены изображения каждые 1 секунду
 setInterval(changeImage, 2000); // 1000 миллисекунд = 1 секунда
+*/
